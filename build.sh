47ff7524a4d1bc36bc43e980cdc0785915f72cf6
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-10-23 16:39:06 +0200

    Correct name

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 VERSION=0.25.0-0.2.70.ubuntu1404
 
-docker build -t containersol/mesos-parent:${VERSION} mesos-parent
+docker build -t containersol/minimesos-parent:${VERSION} mesos-parent
 docker build -t containersol/mesos-master:${VERSION} master
 docker build -t containersol/mesos-agent:${VERSION} agent
 
