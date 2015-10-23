b63c7f9cfa5d14b821856a948f4061fc7af534c8
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-10-23 12:58:23 +0200

    Parent images

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+VERSION=0.25.0-0.2.70.ubuntu1404
+
+docker build -t containersol/mesos-parent:${VERSION} mesos-parent
+docker build -t containersol/mesos-master:${VERSION} master
+docker build -t containersol/mesos-agent:${VERSION} agent
+
+docker build -t containersol/jre8-docker jre8-docker
