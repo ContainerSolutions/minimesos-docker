9466fefddaa7468af2b305869f926dd6ae88fd7c
Remmelt Pit <remmelt.pit@container-solutions.com>
2016-03-22 12:25:46 +0100

    Script now exits when command fails

--- build.sh
+++ build.sh
@@ -5,17 +5,19 @@ buildVersion() {
     docker build \
       -t containersol/mesos-${i}:$1 \
       -f $1/agent/Dockerfile \
-      .
+      . || exit $?
   done
 }
 
-docker build -t containersol/alpine3.3-java8-jre:v1 alpine3.3-java8-jre
+docker build \
+  -t containersol/alpine3.3-java8-jre:v1 \
+  alpine3.3-java8-jre || exit $?
 
 # TODO this should not be "latest"
 docker build \
   -t containersol/mesos-base:latest \
   -f base/Dockerfile \
-  .
+  . || exit $?
 
 buildVersion "0.25.0-0.2.70.ubuntu1404"
 buildVersion "0.26.0-0.2.145.ubuntu1404"
