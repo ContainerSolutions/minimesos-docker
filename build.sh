d688a167ea7440ffe1ced7d35e54dfb5e978d324
Frank Scholten <frank@frankscholten.nl>
2016-07-14 15:36:44 +0200

    Build and push scripts now create separate Master and Agent images based on Mesos version and project semver.

--- build.sh
+++ build.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-buildVersion() {
+if [ "${MINIMESOS_DOCKER_VERSION}" == "" ]
+then
+  echo "Please set MINIMESOS_DOCKER_VERSION before building"
+  exit 1
+fi
+
+buildImage() {
+  MESOS_VERSION=$1
+  MESOSPHERE_TAG=$2
+
   for role in agent master; do
     echo
-    echo Building containersol/mesos-${role}:${1}
+    echo Building containersol/mesos-${role}-${MESOS_VERSION}:${MINIMESOS_DOCKER_VERSION}
     docker build \
-      -t containersol/mesos-${role}:${1} \
+      -t containersol/mesos-${role}-${MESOS_VERSION}:${MINIMESOS_DOCKER_VERSION} \
       -f mesos-image/${role}/Dockerfile \
-      --build-arg MESOS_VERSION=${1} \
+      --build-arg MESOS_VERSION=${MESOS_VERSION}-${MESOSPHERE_TAG} \
+      . || exit $?
+    docker build \
+      -t containersol/mesos-${role}-${MESOS_VERSION}:latest \
+      -f mesos-image/${role}/Dockerfile \
+      --build-arg MESOS_VERSION=${MESOS_VERSION}-${MESOSPHERE_TAG} \
       . || exit $?
   done
 }
@@ -17,13 +31,14 @@ docker build \
   alpine3.3-java8-jre || exit $?
 
 docker build \
-  -t containersol/mesos-base:0.1.0 \
+  -t containersol/mesos-base:${MINIMESOS_DOCKER_VERSION} \
   -f base/Dockerfile \
   . || exit $?
 
-buildVersion "0.25.0-0.2.70.ubuntu1404"
-buildVersion "0.26.0-0.2.145.ubuntu1404"
-buildVersion "0.27.0-0.2.190.ubuntu1404"
-buildVersion "0.27.1-2.0.226.ubuntu1404"
-buildVersion "0.28.0-2.0.16.ubuntu1404"
-buildVersion "0.28.1-2.0.20.ubuntu1404"
+#          Mesos version  Mesosphere tag
+buildImage "0.25.0"       "0.2.70.ubuntu1404"
+buildImage "0.26.0"       "0.2.145.ubuntu1404"
+buildImage "0.27.0"       "0.2.190.ubuntu1404"
+buildImage "0.27.1"       "2.0.226.ubuntu1404"
+buildImage "0.28.0"       "2.0.16.ubuntu1404"
+buildImage "0.28.1"       "2.0.20.ubuntu1404"
--- push.sh
+++ push.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-echo Pushing containersol/mesos-base:0.1.0
-docker push containersol/mesos-base:0.1.0 || exit $?
+if [ "${MINIMESOS_DOCKER_VERSION}" == "" ]
+then
+  echo "Please set MINIMESOS_DOCKER_VERSION before building"
+  exit 1
+fi
+
+echo Pushing containersol/mesos-base:${MINIMESOS_DOCKER_VERSION}
+docker push containersol/mesos-base:${MINIMESOS_DOCKER_VERSION} || exit $?
 
 pushImage() {
+  MESOS_VERSION=$1
+  MESOSPHERE_TAG=$2
+
   for role in agent master; do
     echo
-    echo Pushing containersol/mesos-${role}:${1}
-    docker push containersol/mesos-${role}:${1} || exit $?
+    echo Pushing containersol/mesos-${role}-${MESOS_VERSION}:${MINIMESOS_DOCKER_VERSION}
+    docker push containersol/mesos-${role}-${MESOS_VERSION}:${MINIMESOS_DOCKER_VERSION} || exit $?
+    docker push containersol/mesos-${role}-${MESOS_VERSION}:latest || exit $?
   done
+
 }
 
-pushImage "0.25.0-0.2.70.ubuntu1404"
-pushImage "0.26.0-0.2.145.ubuntu1404"
-pushImage "0.27.0-0.2.190.ubuntu1404"
-pushImage "0.27.1-2.0.226.ubuntu1404"
-pushImage "0.28.0-2.0.16.ubuntu1404"
-pushImage "0.28.1-2.0.20.ubuntu1404"
+#            Mesos version  Mesosphere tag
+pushImage "0.25.0"       "0.2.70.ubuntu1404"
+pushImage "0.26.0"       "0.2.145.ubuntu1404"
+pushImage "0.27.0"       "0.2.190.ubuntu1404"
+pushImage "0.27.1"       "2.0.226.ubuntu1404"
+pushImage "0.28.0"       "2.0.16.ubuntu1404"
+pushImage "0.28.1"       "2.0.20.ubuntu1404"
 
